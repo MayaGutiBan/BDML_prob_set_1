5c90b89ae34d40df5e5468b465d2e3f7fd163345
MayaGutiBan <gubita@icloud.com>
2025-02-26 17:21:53 -0500

    Update Gender_GAP.R

--- Scripts/Gender_GAP.R
+++ Scripts/Gender_GAP.R
@@ -1,35 +1,37 @@
-###### Gender GAP Model 
+# 4. The gender earnings GAP ---------------------------------------------------------------
+#Unconditional wage gap
+# Fit the regression model
+uncon_model <- lm(db_geih$log_ingtot ~ db_geih$female)
+summary(uncon_model)
 
-### We find employed persons with wage = 0, so we erease those observations 
+# Tidy output with p-values and confidence intervals
+tidy(uncon_model)
 
-df_0 <- df %>% filter(ingtot > 0)
+#Conditional wage gap model
+# 4bi. FWL
+con_model <- lm(log_ingtot ~ female + age + I(age^2) + cuentaPropia + estrato1 +
+                  formal + maxEducLevel + parentesco_jhogar + tiempo_trabajando +
+                  otro_trabajo + relab + sizeFirm + totalHoursWorked, data = db_geih)
+                  
+#7495 El mes pasado, ¿recibió pagos por concepto de arriendos y/o pensiones?
+#college
+#oficio
+stargazer(con_model,type="text",digits=7)                
+                  
+# 4bi. FWL with bootstrap
+db_geih$female <- as.numeric(as.character(db_geih$female))  # Convert factor to numeric
+res_female <- lm(female ~ age + I(age^2) + cuentaPropia + estrato1 +
+                   formal + maxEducLevel + parentesco_jhogar + tiempo_trabajando +
+                   otro_trabajo + relab + sizeFirm + totalHoursWorked, data = db_geih)$residuals
+#This isolates the variation in female that is not explained by the other covariates.
 
-low <- quantile(df_0$ln_Inc, 0.01)
-up <- quantile(df_0$ln_Inc, 0.99)
+res_log_ingtot <- lm(log_ingtot ~ age + I(age^2) + cuentaPropia + estrato1 +
+                       formal + maxEducLevel + parentesco_jhogar + tiempo_trabajando +
+                       otro_trabajo + relab + sizeFirm + totalHoursWorked, 
+                     data = db_geih)$residuals
+#This isolates the variation in log_ingtot that is not explained by the same set of independent variables.
 
+fwl_model <- lm(res_log_ingtot ~ res_female)
+summary(fwl_model)
 
-d <-ggplot(data= df_0, 
-          mapping = aes(y=ln_Inc , x="")) +
-  theme_bw() +
-  geom_boxplot()  +
-  ggtitle("")+
-  ylab("Ingreso Total")+
-  xlab("")
-
-
-d <- d + geom_hline(yintercept = low ,linetype="solid",color="red",linewidth=0.7) +
-  geom_hline(yintercept = up ,linetype="solid",color="red",linewidth=0.7)
-
-print(d)
-
-# Create the log of the wage 
-df<- df %>%
-  mutate(ln_wage = log(ingtot) )
-
-#Create the model 
-
-Model2 <- lm(ln_wage ~ sex, data = df_0)
-summary(Model2)
-
-beta_2 <- coef(Model2)
-
+stargazer(con_model,fwl_model,type="text",digits=7)

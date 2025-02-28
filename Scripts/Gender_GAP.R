68ceb76e3cc3adff235f10639de446fbbd41aacf
carlosmanpipe <84484424+carlosmanpipe@users.noreply.github.com>
2025-02-28 16:08:19 -0500

    Update Gender_GAP.R

--- Scripts/Gender_GAP.R
+++ Scripts/Gender_GAP.R
@@ -35,3 +35,44 @@ fwl_model <- lm(res_log_ingtot ~ res_female)
 summary(fwl_model)
 
 stargazer(con_model,fwl_model,type="text",digits=7)
+### 4c. plot the predicted age-wage profile and estimate the implied “peak ages” with the respective confidence intervals 
+
+
+## predict log inc 
+
+db_geih_1$predict_loginc <- predict(con_model, newdata = db_geih_1)
+
+# Function to estimate peak age in each bootstrap sample 
+peak_age_boot <- function(data, indices, gender) {
+  d <- data[indices, ]
+  
+  # Subset data by gender (0 = male, 1 = female)
+  d_gender <- d[d$female == gender, ]
+  
+  model <- lm(
+    log_ingtot ~ age + I(age^2) + cuentaPropia + estrato1 + 
+      formal + maxEducLevel + parentesco_jhogar + tiempo_trabajando + 
+      otro_trabajo + relab + sizeFirm + totalHoursWorked, 
+    data = d_gender
+  )
+  
+  coef_model <- coef(model)
+  b_age <- coef_model["age"]
+  b_age_sq <- coef_model["I(age^2)"]
+  
+  # Calculate peak age (-b_age / (2*b_age_sq))
+  peak_age <- -b_age / (2 * b_age_sq)
+  return(peak_age)
+}
+
+# Run bootstrap for males and females
+B <- 1000
+boot_male <- boot(db_geih_1, statistic = peak_age_boot, R = B, gender = 0)
+boot_female <- boot(db_geih_1, statistic = peak_age_boot, R = B, gender = 1)
+
+# Get confidence intervals
+ci_male <- boot.ci(boot_male, type = "perc")
+ci_female <- boot.ci(boot_female, type = "perc")
+
+print(ci_male)
+print(ci_female)

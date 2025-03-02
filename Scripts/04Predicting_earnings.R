676c19428b0b275c611ccbb1ed131145c0068d4b
Juan Felipe Triana <jf.trianag@uniandes.edu.co>
2025-03-02 14:50:27 -0500

    Forth commit

--- Scripts/04Predicting_earnings.R
+++ Scripts/04Predicting_earnings.R
@@ -1,3 +1,37 @@
+##########################################################
+#Problem set 1: predicting income
+#Authors: Grupo 12
+#Script description: Predicting earnings
+
+##########################################################
+
+# Clean the workspace -----------------------------------------------------
+rm(list=ls())
+cat("\014")
+local({r <- getOption("repos"); r["CRAN"] <- "http://cran.r-project.org"; options(repos=r)}) #set repo
+
+
+# Load Packages -----------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+p_load(rio, # import/export data
+       tidyverse, # tidy-data
+       caret, # For predictive model assessment
+       gridExtra, # arrange plots
+       skimr, # summarize data 
+       stargazer, #model viz and descriptive statistics
+       rvest,
+       ggcorrplot,
+       ggplot2,
+       broom,
+       knitr,
+       mosaic,
+       stats,
+       boot
+) 
+
+# Cargar datos 
+db_geih <- read_rds("stores/clean_GEIH.rds")
+
 # 5. Predicting earnings ---------------------------------------------------------------
 
 #Testing and training
@@ -23,7 +57,7 @@ split_data <- data.frame(
 )
 
 # Create the visualization
-ggplot(split_data, aes(x = Split, y = Count)) +
+train_test <- ggplot(split_data, aes(x = Split, y = Count)) +
   geom_bar(stat = "identity", fill = "darkblue", width = 0.5) +
   geom_text(aes(label = paste0(round(Percentage, 1), "%\n(n=", Count, ")")), 
             vjust = -0.5, color = "black", size = 4) +
@@ -32,6 +66,8 @@ ggplot(split_data, aes(x = Split, y = Count)) +
        x = "") +
   theme_bw() +
   ylim(0, max(split_data$Count) * 1.2)  # Add some space for labels
+
+ggsave("views/train_test.png", plot = train_test, width = 8, height = 6, dpi = 300)
 
 #Train models
 
@@ -49,7 +85,7 @@ formulas <- list(
     formal + maxEducLevel + parentesco_jhogar + tiempo_trabajando +
     otro_trabajo + relab + sizeFirm + totalHoursWorked + totalHoursWorked*sizeFirm
 ) # Interaction effect
-)
+
 
 # Initialize a list to store RMSE scores
 rmse_scores <- data.frame(Model = character(), RMSE = numeric(), stringsAsFactors = FALSE)
@@ -88,15 +124,26 @@ predictions <- predict(form_7, newdata = test_data)
 errors <- test_data$log_ingtot - predictions
 
 ## show distribution of erros
-hist(errors, main = "Distribution of Prediction Errors from 
-     log(total income)", xlab = "Error", col = "lightblue", breaks = 40)
+df_errors <- data.frame(errors = errors)
+
+# Crear el histograma con ggplot
+dist_errors <- ggplot(df_errors, aes(x = errors)) +
+  geom_histogram(fill = "lightblue", bins = 40, color = "black") +
+  labs(title = "Distribution of Prediction Errors from log(total income)",
+       x = "Error", 
+       y = "Frequency") +
+  theme_bw()
+
+# Guardar la imagen en la carpeta views
+ggsave("views/dist_errors.png", plot = dist_errors, width = 8, height = 6, dpi = 300)
+
 
 ## look for outliers 
 ## we look for individuals wich observed log_inctot is smaller than its predicted ones 
 low <- quantile(errors, 0.001) 
 
 
-ggplot(data = data.frame(errors), aes(x = errors)) +
+dist_errors_q1 <- ggplot(data = data.frame(errors), aes(x = errors)) +
   geom_histogram(binwidth = 0.2, fill = "lightblue", color = "black") +
   labs(title = "Distribution of Prediction Errors",
        x = "Error",
@@ -104,7 +151,7 @@ ggplot(data = data.frame(errors), aes(x = errors)) +
   theme_minimal() + 
   geom_vline(aes(xintercept = low, linetype = "0.1% percentile"), color = "lightblue", linewidth = 1) 
 
-
+ggsave("views/dist_errors_q1.png", plot = dist_errors_q1, width = 8, height = 6, dpi = 300)
 
 
 
@@ -126,7 +173,7 @@ cat("Starting LOOCV training with", n_obs, "iterations...\n")
 # Train model with progress printing
 ctrl$verboseIter <- TRUE  # Enable progress printing
 modelo4d <- train(form_4,
-                  data = db,
+                  data = db_geih,
                   method = 'lm', 
                   trControl = ctrl)
 

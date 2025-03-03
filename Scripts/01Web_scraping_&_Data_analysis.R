2e7eacd7ae660b32eafb1f2b4043f5b22f179bc3
carlosmanpipe <84484424+carlosmanpipe@users.noreply.github.com>
2025-03-03 08:09:29 -0500

    Update 01Web_scraping_&_Data_analysis.R

--- Scripts/01Web_scraping_&_Data_analysis.R
+++ Scripts/01Web_scraping_&_Data_analysis.R
@@ -101,14 +101,8 @@ db_geih <- read.csv("stores/clean_GEIH.csv")
 head(db_geih, 5)
 
 variables <- c("age", "cuentaPropia", "estrato1", "formal", "ingtot", "maxEducLevel", "p6050", "p6426", "p7040", "p7495", "relab", "sex", "sizeFirm", 
-              "totalHoursWorked", "nmenores", "oficio", "college", "regSalud")
+              "totalHoursWorked", "nmenores", "oficio", "college", "regSalud","y_salary_m", "y_salary_m_hu", "y_ingLab_m", "y_primaServicios_m", "y_ingLab_m_ha", "y_total_m", "y_total_m_ha")
 
-variables_ing <- c("ingtot", "y_salary_m", "y_salary_m_hu", "y_ingLab_m", "y_primaServicios_m", "y_ingLab_m_ha", "y_total_m", "y_total_m_ha")
-
-db_geih_ing <- db_geih %>%  
-  select(variables_ing)
-
-vis_dat(db_geih_ing)
 
 stargazer(as.data.frame(db_geih[, c("clase", "depto")]), type = "text", summary = TRUE)
 
@@ -125,7 +119,6 @@ db_geih_1 <- db_geih %>%
   rename( parentesco_jhogar = "p6050",
           tiempo_trabajando = "p6426", # la variable esta en meses
           otro_trabajo = "p7040",
-          otro_ingreso = "p7495",
           gender = "sex") %>% 
   mutate( female = 1-gender, ## 1 if female
           H_Head = ifelse( parentesco_jhogar== 1, 1, 0)) #Household head
@@ -170,6 +163,12 @@ db_geih_1 <- db_geih_1 %>%
   ungroup() %>% 
   mutate(ingtot = ifelse(ingtot==0, yes =mean_ingtot, no=ingtot),
          log_ingtot = log(ingtot)) 
+
+
+## we save the final db 
+write_rds(db_geih_1,"stores/GEIH_final.rds")
+
+### DATA VISUALIZATION ---------
 
 # Function to create a single boxplot
 create_boxplot <- function(data, y_var, y_label) {
@@ -267,5 +266,4 @@ cor_matrix <- cor(M, use = "pairwise.complete.obs")  # Compute correlation matri
 corrplot(cor_matrix, method = "circle")
 ggsave("views/corr_plot_2.png", plot = corr_plot_2, width = 8, height = 6, dpi = 300)
 
-# Guardar data limpia
-write_rds(db_geih_1,"stores/clean_GEIH.rds")
+
